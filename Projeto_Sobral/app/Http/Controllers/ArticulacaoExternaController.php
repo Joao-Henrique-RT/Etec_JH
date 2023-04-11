d61ee259bfcadab8fcd3fa7bc605a088ec6e4259
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2023-04-11 19:47:35 -0300

    att

---
+++ Projeto_Sobral/app/Http/Controllers/ArticulacaoExternaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticulacaoExternaController extends Controller
+{
+    public function index(){
+        return view('Cood_Gest_pedagogica.Articulacao_Pedagogica');
+    }
+}
---
+++ Projeto_Sobral/resources/views/Cood_Gest_Pedagogica/Articulacao_Pedagogica.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Articulação pedagógica</title>
+</head>
+<body>
+    
+    <h1>CÉLULA DA SUPERVISÃO E ARTICUAÇÃO PEDAGÓGICA</h1>
+
+    <a href=" {{ route('Cood_Gest_Pedagogica.Cood_Gest_Pedagogica') }}">COORDENADORIA GESTÃO PEDAGÓGICA</a>
+
+
+
+
+</body>
+</html>
